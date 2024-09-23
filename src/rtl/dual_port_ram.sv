d77a19059dbbb47fc0785167e4fde2aa4c63fa44
wonjongbot <wonjongbot@gmail.com>
2024-09-23 20:57:31 +0900

    syncronous reset for ram

--- src/rtl/dual_port_ram.sv
+++ src/rtl/dual_port_ram.sv
@@ -1,26 +1,27 @@
 `include "rv32-ooo_soc.svh"
 
-module dual_port_ram
-  #(
+module dual_port_ram #(
     parameter num_entries = 512,
-    parameter addr_width = $clog2(num_entries),
-    parameter data_width = 2
-  )(
-    input we, clk,
+    parameter addr_width  = $clog2(num_entries),
+    parameter data_width  = 2
+) (
+    input we, clk, rst,
     input [addr_width-1:0] waddr, raddr,
     input [data_width-1:0] wdata,
     output reg [data_width-1:0] q
-  );
-  reg [data_width-1:0] mem [0:num_entries-1];
-  
+);
+  reg [data_width-1:0] mem [num_entries];
+
   initial begin
     $readmemb({`PROJECT_ROOT, "src/data/tage_t0_init.bin"}, mem);
   end
-  
-  always @(posedge clk)
-    begin
-      if(we)
-        mem[waddr] <= wdata;
-      q<=mem[raddr];
+
+  always @(posedge clk) begin
+    if(rst)
+      q<= 0;
+    else begin
+      if (we) mem[waddr] <= wdata;
+      q <= mem[raddr];
     end
+  end
 endmodule
